50c15aa5b2c2990b3b8e0a60d0517dccbc9d6d7f
tony <tonypierrekim@gmail.com>
2024-12-20 03:39:11 -0500

    메롱

--- test/kunasdui_test.dart
+++ test/kunasdui_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:kunasdui/kunasdui.dart';
@@ -8,5 +9,18 @@ void main() {
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
+
+    void incrementCounter() => ();
+    Map<String, dynamic> jsonString = {
+      "object": {
+        "type": "button",
+        "methods": {"onPressed": incrementCounter},
+        "tooltip": "increment",
+      }
+    };
+    var jsonStringObject = jsonString["object"];
+    final myUI = UIObject.fromJson(jsonStringObject);
+    expect(myUI.toButton().onPressed.hashCode,
+        FloatingActionButton(onPressed: incrementCounter).onPressed.hashCode);
   });
 }
